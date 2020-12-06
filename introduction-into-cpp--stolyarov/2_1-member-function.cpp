df8d5f83a63684538cd0cab0d21a9e8d477932bf
Maksim Kalenich <maxim.kalenich@gmail.com>
2020-12-06 20:09:35 +0300

    chore: change constructor

--- introduction-into-cpp--stolyarov/2_1-member-function.cpp
+++ introduction-into-cpp--stolyarov/2_1-member-function.cpp
@@ -7,16 +7,18 @@ using namespace std;
 
 struct complex_num {
 private:
-    double mod, arg;
+    double re, im, mod, arg;
 public:
-    complex_num(double re, double im) {
+    complex_num(double a_re, double a_im) {
+        re = a_re;
+        im = a_im;
         mod = sqrt(re * re + im * im);
         arg = atan2(im, re);
     }
 
-    double get_re() { return mod * cos(arg); }
+    double get_re() { return re; }
 
-    double get_im() { return mod * sin(arg); }
+    double get_im() { return im; }
 
     double modulo() { return mod; }
 
