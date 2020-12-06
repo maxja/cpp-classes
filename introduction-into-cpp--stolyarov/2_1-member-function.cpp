f8790cb1574474c7f405e4302f34cca1337f43e7
Maksim Kalenich <maxim.kalenich@gmail.com>
2020-12-06 20:57:51 +0300

    feat!: replace structure with class

--- introduction-into-cpp--stolyarov/2_1-member-function.cpp
+++ introduction-into-cpp--stolyarov/2_1-member-function.cpp
@@ -3,31 +3,26 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
-struct complex_num {
-private:
-    double re, im, mod, arg;
+class Complex {
+    double re, im;
 public:
-    complex_num(double a_re, double a_im) {
+    Complex(double a_re, double a_im) {
         re = a_re;
         im = a_im;
-        mod = sqrt(re * re + im * im);
-        arg = atan2(im, re);
     }
 
     double get_re() { return re; }
 
     double get_im() { return im; }
 
-    double modulo() { return mod; }
+    double modulo() { return sqrt(re * re + im * im); }
 
-    double argument() { return arg; }
+    double argument() { return atan2(im, re); }
 };
 
 int main() {
     double mod;
-    mod = complex_num(2.7, 3.8).modulo();
+    mod = Complex(2.7, 3.8).modulo();
 
     std::cout << mod << std::endl;
 
